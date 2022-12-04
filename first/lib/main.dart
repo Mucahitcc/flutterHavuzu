192c2a7f4176c3a40d48ee4eb3be09625c10d38d
Mucahitcc <mucahitcihatcop@gmail.com>
2022-12-04 22:31:18 +0300

    first push

---
+++ first/lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    BenimUyg(),
+  );
+}
+
+class BenimUyg extends StatelessWidget {
+  const BenimUyg({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.brown[300],
+        body: SafeArea(
+          child: Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 70.0,
+                  backgroundColor: Colors.lime,
+                  backgroundImage: AssetImage('assets/images/kahve.jpg'),
+                ),
+                Text(
+                  'Flutter Kahvecisi',
+                  style: TextStyle(
+                    fontFamily: 'Satisfy',
+                    fontSize: 45,
+                    color: Colors.brown[900],
+                  ),
+                ),
+                Text(
+                  'Bir Fincan Uzağınızda',
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ untitled/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Color(0xffD7E0FF),
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.blueGrey[100],
+        title: Text('Mutlu Bayramlar'),
+      ),
+      body: Center(child: Image.asset('images/mutlu_bayramlar.jpg')),
+    ),
+  ));
+}
